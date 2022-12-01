62e1a4cb228e0a77248db7283cc5d1d7fe68a621
Simon <simon.vincent1@ucalgary.ca>
2022-12-01 14:50:54 -0700

    Revamped styling to be more dyamic in chat

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -35,9 +35,17 @@ const Chat = ({ socket }) => {
 
         <p className='text-black-50 text-start ms-2 mt-3 negative-margin-bottom'>Conversations</p>
 
-        <Container className="border-top" id="chat-container">
-          <Row>
-            <Col xs={2}><Image className="roundedCircle"></Image></Col>
+        <Container id="chat-container">
+          {/* Use this row's styling for profile results */}
+          <Row className='border-top align-items-center'>
+            <Col xs={2}><Image src="default_prof.png" className="align-middle rounded-circle" width="50"></Image></Col>
+            <Col xs={1}></Col>
+            <Col xs={5}><p className="mb-0 pt-3 fw-bold">Name</p><p className="small fw-lighter text-muted">Time</p></Col>
+            <Col><Badge bg="secondary">1</Badge></Col>
+            <Col xs={1}><ChevronRight color="black" size={20}></ChevronRight></Col>
+          </Row>
+          <Row className='border-top'>
+            <Col xs={2}><Image src="default_prof.png" className="align-middle rounded-circle" width="50"></Image></Col>
             <Col xs={1}></Col>
             <Col xs={5}><p className="text-start pt-1 mb-0 fw-bold">Name</p><p className="small text-start fw-lighter text-muted">Time</p></Col>
             <Col><Badge className="mt-3" bg="secondary">1</Badge></Col>

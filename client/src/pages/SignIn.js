54b93092c0318afd6e4913b4b9c828feeca5b1d9
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-04 18:44:54 +0000

    sign in page + fake sign in logic working

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -7,17 +7,43 @@ import {
   Row
 } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 const SignIn = ({ socket }) => {
 
-  return (
-    // 'html' code goes here 
-    <>
-      <Container>
+  const handleSignin = async (e) => {
+    e.preventDefault()
+    const email = e.target.emailInput.value
+    const password = e.target.passwordInput.value
 
-        <h1>SignIn page here</h1>
-        <Card>
-          <Button variant="danger" >Bootstrap button</Button>
+    const response = await axios.post('http://localhost:4000/signin', {
+      email,
+      password
+    })
+
+    console.log(response.data);
+  }
+
+
+  return (
+    <>
+      <Container className='container-fluid'>
+        <h1 className="text-start fw-bold mt-4 ms-3">Sign In</h1>
+        <Card className="mx-3">
+          <Card.Body>
+            <Card.Title className="text-start">Sign In</Card.Title>
+            <form onSubmit={handleSignin}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="passwordInput" />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </Card.Body>
         </Card>
       </Container>
     </>

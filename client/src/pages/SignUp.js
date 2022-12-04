9e9b2692391ac75f50a780429c845daa0b7a3aa8
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-04 18:01:49 +0000

    sign up fields

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -7,20 +7,49 @@ import {
   Row
 } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 const SignIn = ({ socket }) => {
 
-  return (
-    <>
-      <Container>
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+    const response = await axios.post('/api/signup', { email, password })
+    console.log(response)
+  }
 
-        <h1>SIgn up page here</h1>
-        <Card>
-          <Button variant="danger" >Bootstrap button</Button>
-        </Card>
-      </Container>
-    </>
-  )
-}
 
-export default SignIn
+    return (
+      <>
+        <Container className='container-fluid'>
+          <h1 className="text-start fw-bold mt-4 ms-3">Sign Up</h1>
+          <Card className="mx-3">
+            <Card.Body>
+              <Card.Title className="text-start">Create an account</Card.Title>
+              <Card.Text className="text-start">
+                {/* <form>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">Must be your school email.</div>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="passwordInput" />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" id="passwordInputConfirm" />
+                  </div>
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form> */}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      </>
+    )
+  }
+
+  export default SignIn

84ab87c346a2bc0a37381d8a3112b901f7b2422b
Simon <simon.vincent1@ucalgary.ca>
2022-12-04 13:17:54 -0700

    Added Components and component styling

---
+++ client/src/components/ChatFooter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Send } from 'react-bootstrap-icons';
+import {
+
+} from 'react-bootstrap';
+import { useNavigate } from "react-router-dom"
+
+const ChatFooter = () => {
+    const [message, setMessage] = useState('');
+
+    const handleSendMessage = (e) => {
+        e.preventDefault();
+        console.log({ userName: localStorage.getItem('userName'), message });
+        setMessage('');
+    };
+    return (
+        <div className="chat__footer">
+            <form className="form" onSubmit={handleSendMessage}>
+                <input
+                    type="text"
+                    placeholder="Write message"
+                    className="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button className="sendBtn"><Send></Send></button>
+            </form>
+        </div>
+    );
+};
+
+export default ChatFooter;

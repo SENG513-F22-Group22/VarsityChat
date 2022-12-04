b4f6affe288bdc558dbb7e8f2b5891ad569598dd
Luke <lukeac3214@gmail.com>
2022-12-04 14:43:13 -0700

    version1

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const cors = require("cors")
 const bodyParser = require("body-parser");
 const { log } = require("console");
 const http = require('http').Server(app);
-const PORT = 4000
+const mongoose = require("mongoose");
+const PORT = 4000;
+const CONNECTION_URL = 'mongodb+srv://SENG513PROJ:oRAMdvj4wDQLlLc7@cluster0.fviwa49.mongodb.net/SENGDB?retryWrites=true&w=majority';
+
+// schema for user data
+const User = mongoose.model('User', {
+  email: { type: String },
+  password: { type: String }
+});
+
 const socketIO = require('socket.io')(http, {
   cors: {
     origin: "http://localhost:3000"
@@ -38,48 +47,54 @@ socketIO.on('connection', (socket) => {
   });
 });
 
-
-const fakeDB = {
-  users: [
-    {
-      username: "tim@ucalgary.ca",
-      password: "password"
-    },
-  ]
-}
-
 app.get("/api", (req, res) => {
-  
+
   res.json({ message: "Hello" })
 });
 
 app.post("/signup", (req, res) => {
   const { email, password } = req.body
-  const user = fakeDB.users.find(user => user.username === email)
-  if (user) {
-    res.json({ error: "User already exists" })
-  } else {
-    fakeDB.users.push({
-      username: email,
-      password
-    })
-    res.json({ message: "User created" })
-  }
+
+  User.find({ email: email }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    else if (docs.length > 0) {
+      res.json({ error: "User already exists" });
+    }
+    else {
+      const newUser = new User({
+        email: email,
+        password: password
+      });
+      newUser.save();
+      console.log("First function call : ", docs, docs[0].email);
+      res.json({ message: "User created" })
+    }
+    
+  });
 })
 
 app.post("/signin", (req, res) => {
   const { email, password } = req.body
-  const user = fakeDB.users.find(user => user.username === email && user.password === password)
+  User.find({ email: email, password: password}, function(err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    else if (docs.length === 1) {
+      res.status(200).json({ message: "User logged in" })
+    }
+    else {
+      res.status(401).json({ error: "Invalid credentials" })
+    }
+  })
 
-  if (user) {
-    res.status(200).json({ message: "User logged in" })
-  } else {
-    res.status(401).json({ error: "Invalid credentials" })
-  }
 });
 
 
 
-http.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+mongoose.connect(CONNECTION_URL)
+  .then(() => http.listen(PORT, () => {
+    console.log(`Successfully Connected to MongoDB Cloud. Server listening on ${PORT}`);
+  }))
+  .catch((error) => console.log(error.message));

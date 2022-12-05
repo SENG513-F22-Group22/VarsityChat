0b28a0159b4dc1a9d17d90d1937c9066a6360673
Simon <simon.vincent1@ucalgary.ca>
2022-12-05 12:53:01 -0700

    Title, search and beginning

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -6,6 +6,7 @@ import {
   Container,
   Row,
   ButtonGroup,
+  Form
 } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom"
 
@@ -15,8 +16,24 @@ const Search = ({ socket }) => {
     // 'html' code goes here 
     <>
       <Container>
+        <Row>
+          <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
+        </Row>
+        <Row>
+          <Form className="d-flex mx-3">
+            <Form.Control
+              type="search"
+              placeholder="Search by Course Name"
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+        </Row>
 
-        <h1>Discover page here</h1>
+        <Container className="mt-4">
+
+        </Container>
+
       </Container>
     </>
   )

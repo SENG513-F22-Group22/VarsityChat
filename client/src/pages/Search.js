fbabdbee72a07b7dfb89ad35c61fa0299c102e60
Luke <lukeac3214@gmail.com>
2022-12-09 20:13:21 -0700

    modified:   client/src/pages/Search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -18,80 +18,79 @@ const Search = ({ socket }) => {
   const navigate = useNavigate()
   return (
     // had issues merging this code (mismatched brackets somewhere)
-      //   // 'html' code goes here 
-      //   <>
-      //   <Container>
-      //     <Row>
-      //       <Col xs={0} lg={3}></Col>
-      //       <Col lg={6}>
-      //         <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
-      //       </Col>
-      //       <Col xs={0} lg={3}></Col>
-      //     </Row>
-  
-      //     <Row>
-      //       <Col xs={0} lg={3}></Col>
-      //       <Col lg={6}>
-      //         <p className='border-bottom text-black-50 text-start ms-2 mt-2 mb-2 negative-margin-bottom'>Enrolled Classes</p>
-      //       </Col>
-      //       <Col xs={0} lg={3}></Col>
-      //     </Row>
-  
-      //     <Row>
-      //       <Col xs={0} lg={3}></Col>
-      //       <Col lg={6}>
-      //         <Container id="SearchCourseContainer" className="mt-2">
-      //           {/* This is where ClassSearchItems are appended */}
-  
-      //           <ClassSearchItem courseName="SENG 513" />
-  
-      //     <Container className="mt-2">
-      //       <ListGroup id="SearchClassSelection" variant="flush">
-      //         {/* This is where ClassSearchItems are appended */}
-      //         <ListGroup.Item>
-      //           <Row className='classSearchItem align-items-center'>
-      //             <Col xs={4}><p className="mt-1 mb-1">SENG 513</p></Col>
-      //             <Col xs={7}></Col>
-      //             <Col xs={1}>
-      //               <ChevronRight className="mb-1" color="black" size={20} />
-      //             </Col>
-      //           </Row>
-      //         </ListGroup.Item>
-      //         <ListGroup.Item><ClassSearchItem courseName="all" /></ListGroup.Item>
-      //       </ListGroup>
-      //     </Container>
-  
-  
-      //   </Container>
-      // </>
     // 'html' code goes here 
     <>
       <Container>
         <Row>
-          <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
+          <Col xs={0} lg={3}></Col>
+          <Col lg={6}>
+            <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
+          </Col>
+          <Col xs={0} lg={3}></Col>
         </Row>
 
-        <p className='border-bottom text-black-50 text-start ms-2 mt-2 mb-2 negative-margin-bottom'>Enrolled Classes</p>
+        <Row>
+          <Col xs={0} lg={3}></Col>
+          <Col lg={6}>
+            <p className='border-bottom text-black-50 text-start ms-2 mt-2 mb-2 negative-margin-bottom'>Enrolled Classes</p>
+          </Col>
+          <Col xs={0} lg={3}></Col>
+        </Row>
 
-        <Container className="mt-2">
-          <ListGroup id="SearchClassSelection" variant="flush">
-            {/* This is where ClassSearchItems are appended */}
-            <ListGroup.Item>
-              <Row className='classSearchItem align-items-center'>
-                <Col xs={4}><p className="mt-1 mb-1">SENG 513</p></Col>
-                <Col xs={7}></Col>
-                <Col xs={1}>
-                  <ChevronRight className="mb-1" color="black" size={20} />
-                </Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item><ClassSearchItem courseName="all" /></ListGroup.Item>
-          </ListGroup>
-        </Container>
+        <Row>
+          <Col xs={0} lg={3}></Col>
+          <Col lg={6}>
+            <Container id="SearchCourseContainer" className="mt-2">
+              {/* This is where ClassSearchItems are appended */}
 
+              <ClassSearchItem courseName="SENG 513" />
+              <ClassSearchItem courseName="all" />
+            </Container>
+          </Col>
+        </Row>
       </Container>
     </>
+
   )
 }
 
 export default Search
+
+// old code from merging
+// {/* <Container className="mt-2">
+// <ListGroup id="SearchClassSelection" variant="flush">
+//   {/* This is where ClassSearchItems are appended */}
+//   <ListGroup.Item>
+//   <ClassSearchItem courseName="SENG 513" />
+//   </ListGroup.Item>
+//   <ListGroup.Item><ClassSearchItem courseName="all" /></ListGroup.Item>
+// </ListGroup>
+// </Container> */}
+
+    // {/* 'html' code goes here 
+    // <>
+    //   <Container>
+    //     <Row>
+    //       <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
+    //     </Row>
+
+    //     <p className='border-bottom text-black-50 text-start ms-2 mt-2 mb-2 negative-margin-bottom'>Enrolled Classes</p>
+
+    //     <Container className="mt-2">
+    //       <ListGroup id="SearchClassSelection" variant="flush">
+    //         {/* This is where ClassSearchItems are appended */}
+    //         <ListGroup.Item>
+    //           <Row className='classSearchItem align-items-center'>
+    //             <Col xs={4}><p className="mt-1 mb-1">SENG 513</p></Col>
+    //             <Col xs={7}></Col>
+    //             <Col xs={1}>
+    //               <ChevronRight className="mb-1" color="black" size={20} />
+    //             </Col>
+    //           </Row>
+    //         </ListGroup.Item>
+    //         <ListGroup.Item><ClassSearchItem courseName="all" /></ListGroup.Item>
+    //       </ListGroup>
+    //     </Container>
+
+    //   </Container>
+    // </> */}

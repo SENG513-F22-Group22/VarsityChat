ddaeb449158880727a5c18ec3bc0009e4d889252
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-10 21:49:53 +0000

    search users

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -18,12 +18,14 @@ import axios from 'axios'
 
 const SearchResults = (props) => {
     const { socket, userEmail } = props
+    const navigate = useNavigate()
     const [users, setUsers] = useState([])
+    const [filteredUsers, setFilteredUsers] = useState([])
 
     useEffect(() => {
         axios.get('http://localhost:4000/users',
-          ).then((res) => {
-            
+        ).then((res) => {
+
             // remove current user from list
             let validUsers = []
             for (let user of res.data) {
@@ -31,71 +33,77 @@ const SearchResults = (props) => {
                     validUsers.push(user)
                 }
             }
-    
-        
             setUsers(validUsers)
-          }).catch((err) => {
+            setFilteredUsers(validUsers)
+        }).catch((err) => {
             console.log(err);
-          })
-      }, [])
+        })
+    }, [])
 
-    const navigate = useNavigate()
+    const handleTyping = (e) => {
+        let filtered = users.filter((user) => {
+            return user.email.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+        setFilteredUsers(filtered)
+    }
+
     return (
         // 'html' code goes here 
-      
-            <Container>
-                <Row>
-                    <Col xs={0} lg={3}></Col>
-                    <Col lg={6}>
-                        <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
-                    </Col>
-                    <Col xs={0} lg={3}></Col>
-                </Row>
-                <Row>
-                    <Col xs={0} lg={3}></Col>
-                    <Col lg={6}>
-                        <Form className="d-flex mx-3">
-                            <Form.Control
-                                type="search"
-                                placeholder="Filter by name"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                        </Form>
-                    </Col>
-                    <Col xs={0} lg={3}></Col>
-                </Row>
-                <Row className="mt-2 ms-1">
-                    <Col xs={0} lg={3}></Col>
-                    <Col xs={4} lg={6}>
-                        <Button variant="outline-secondary" onClick={() => navigate("/search")}>
-                            <ArrowLeft color="black" size={20} />
-                        </Button>
-                    </Col>
-                    <Col xs={0} lg={6}></Col>
-                </Row>
 
-                <Row>
-                    <Col xs={0} lg={3}></Col>
-                    <Col lg={6}>
-                        <p className='border-bottom text-black-50 text-start ms-3 mt-2 mb-2 negative-margin-bottom'>Course Name</p>
-                    </Col>
-                    <Col xs={0} lg={3}></Col>
-                </Row>
+        <Container>
+            <Row>
+                <Col xs={0} lg={3}></Col>
+                <Col lg={6}>
+                    <h2 className="text-start fw-bold mt-4 ms-3">Search</h2>
+                </Col>
+                <Col xs={0} lg={3}></Col>
+            </Row>
+            <Row>
+                <Col xs={0} lg={3}></Col>
+                <Col lg={6}>
+                    <Form className="d-flex mx-3">
+                        <Form.Control
+                            type="search"
+                            placeholder="Filter by name"
+                            className="me-2"
+                            aria-label="Search"
+                            onChange={handleTyping}
+                        />
+                    </Form>
+                </Col>
+                <Col xs={0} lg={3}></Col>
+            </Row>
+            <Row className="mt-2 ms-1">
+                <Col xs={0} lg={3}></Col>
+                <Col xs={4} lg={6}>
+                    <Button variant="outline-secondary" onClick={() => navigate("/search")}>
+                        <ArrowLeft color="black" size={20} />
+                    </Button>
+                </Col>
+                <Col xs={0} lg={6}></Col>
+            </Row>
 
-                <Row>
-                    <Col xs={0} lg={3}></Col>
-                    <Col lg={6}>
-                <Container id="SearchResultsContainer" >
-                    {/* This is where SearchProfileItems are appended */}
-                    {users.map((user) => (
-                        <SearchProfileItem userEmail={userEmail} email={user.email} key={user._id} />
-                    ))}
-                     </Container>
-                    </Col>
-                    <Col xs={0} lg={3}></Col>
-                </Row>
-                </Container>
+            <Row>
+                <Col xs={0} lg={3}></Col>
+                <Col lg={6}>
+                    <p className='border-bottom text-black-50 text-start ms-3 mt-2 mb-2 negative-margin-bottom'>Course Name</p>
+                </Col>
+                <Col xs={0} lg={3}></Col>
+            </Row>
+
+            <Row>
+                <Col xs={0} lg={3}></Col>
+                <Col lg={6}>
+                    <Container id="SearchResultsContainer" >
+                        {/* This is where SearchProfileItems are appended */}
+                        {filteredUsers.map((user) => (
+                            <SearchProfileItem userEmail={userEmail} email={user.email} key={user._id} />
+                        ))}
+                    </Container>
+                </Col>
+                <Col xs={0} lg={3}></Col>
+            </Row>
+        </Container>
     )
 }
 

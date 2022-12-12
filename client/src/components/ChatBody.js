8efbbbffe73b2fba63c4ebbcdebbce896fd8455e
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-12 18:16:48 +0000

    delete button

--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -4,6 +4,8 @@ import {
     Row,
     Col,
     Image,
+    Dropdown,
+    DropdownButton,
 } from 'react-bootstrap';
 import { ArrowLeft } from 'react-bootstrap-icons';
 import { useNavigate } from "react-router-dom"
@@ -38,15 +40,15 @@ const ChatBody = (props) => {
                 console.log(err);
             }).then(
                 axios.get('http://localhost:4000/getRecv',
-                {
-                    params: {
-                        email: userEmail,
-                        room: window.location.href.split('?')[1].split('=')[1],
-                    }
-                })
-                .then((res) => {
-                   setRecipient(res.data)
-                })
+                    {
+                        params: {
+                            email: userEmail,
+                            room: window.location.href.split('?')[1].split('=')[1],
+                        }
+                    })
+                    .then((res) => {
+                        setRecipient(res.data)
+                    })
             )
     }, [])
 
@@ -67,12 +69,29 @@ const ChatBody = (props) => {
         navigate('/chat');
     };
 
+    const handleDeleteChat = () => {
+        axios.delete('http://localhost:4000/deleteChat',
+            {
+                params: {
+                    room: window.location.href.split('?')[1].split('=')[1],
+                    email: userEmail
+                },
+            })
+            .then((res) => {
+                navigate('/chat');
+            }).catch((err) => {
+                console.log(err);
+                alert('Error deleting chat');
+            })
+    };
+
+
     return (
         <>
             <Row>
                 <header className="chat__mainHeader">
                     <Col xs={2} lg={2}>
-                        <Button className="ms-2" variant="outline-secondary" onClick={() => navigate("/chat")}>
+                        <Button className="ms-2" variant="outline-secondary" onClick={handleLeaveChat}>
                             <ArrowLeft color="black" size={20} />
                         </Button>
                     </Col>
@@ -83,7 +102,18 @@ const ChatBody = (props) => {
                         <p className='mt-3 fw-bold'>{recipient}</p>
                     </Col>
                     <Col xs={5} lg={5}></Col>
-                    <Col xs={1} lg={1}><ThreeDotsVertical size={20} color="grey"></ThreeDotsVertical></Col>
+                    <Col xs={1} lg={1}>
+                        {/* Make a dropdown with ThreeDotsVertical */}
+                        <DropdownButton
+                            alignRight
+                            title={<ThreeDotsVertical />}
+                            id="dropdown-menu-align-right"
+                            variant="outline-secondary"
+                            className="mt-2"
+                        >
+                            <Dropdown.Item eventKey="1" onClick={handleDeleteChat}>Delete Chat</Dropdown.Item>
+                        </DropdownButton>
+                    </Col>
                 </header>
             </Row>
             <div className="message__container">
